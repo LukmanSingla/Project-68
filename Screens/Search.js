f40dc144cfadcebed0c035784203f4072f244f61
LukmanSingla <79403472+LukmanSingla@users.noreply.github.com>
2021-09-20 16:19:44 +0530

    Delete Search.js

--- Screens/Search.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-
-export default class SearchScreen extends React.Component {
-  render() {
-    return <Text>Search Screen</Text>;
-  }
-}
